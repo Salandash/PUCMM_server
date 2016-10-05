c8bd28dbe7209048ad7ee8810bead7ac159d6dcc
William <wjhg03@gmail.com>
2016-10-05 17:36:15 -0400

    msg

---
+++ server/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path;
+            int port;
+
+            if (args.GetLength(0) > 0)
+            {
+                path = args[1];
+                port = Convert.ToInt16(args[4]);
+            }
+            else
+            {
+                path = System.Reflection.Assembly.GetEntryAssembly().Location;
+                port = 80;
+
+            }
+
+            Console.WriteLine("PATH: " + path);
+            Console.WriteLine("POTR: " + port);
+        }
+    }
+}
