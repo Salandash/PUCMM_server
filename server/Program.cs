d7012ee1da0bd5d1b90a6f37fda72b4845771be8
William <wjhg03@gmail.com>
2016-10-05 18:24:00 -0400

    Made it so it can change one of the two params

--- server/Program.cs
+++ server/Program.cs
@@ -8,26 +8,50 @@ namespace server
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
             string path;
             int port;
 
             if (args.GetLength(0) > 0)
             {
-                path = args[1];
-                port = Convert.ToInt16(args[3]);
+                if (args[0] == "--path")
+                {
+                    path = args[1];
+                    port = 80;
+                }
+
+                else if (args[0] == "--port")
+                {
+                    port = Convert.ToInt16(args[1]);
+                    path = System.Reflection.Assembly.GetEntryAssembly().Location;
+                }
+
+                else if (args[0] == "--path" && args[2] == "--port")
+                {
+                    path = args[1];
+                    port = Convert.ToInt16(args[3]);
+                }
+
+                else if (args[2] == "--path" && args[0] == "--port")
+                {
+                    path = args[3];
+                    port = Convert.ToInt16(args[1]);
+                }
+                else
+                {
+                    path = args[1];
+                    port = Convert.ToInt16(args[3]);
+                }
             }
             else
             {
                 path = System.Reflection.Assembly.GetEntryAssembly().Location;
                 port = 80;
-
             }
-
             Console.WriteLine("PATH: " + path);
-            Console.WriteLine("POTR: " + port);
-            Console.ReadLine();
+            Console.WriteLine("PORT: " + port);
+            return 0;
         }
     }
 }
