b2f8d95ba42afe6ac7a38ded2fc0d6986abb3f7e
William <wjhg03@gmail.com>
2016-10-10 18:33:10 -0400

    Included HttpClient files

---
+++ server/ClientState.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server
+{
+    class ClientState
+    {
+        enum clientState
+        {
+            ReadingProlog,
+            ReadingHeaders,
+            ReadingContent,
+            PrintingContent,
+            WritingHeaders,
+            WritingContent,
+            Closed
+        }
+    }
+}
