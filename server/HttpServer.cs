9a01baed42fdc79258b8ea7cc1202e295f38475f
William <wjhg03@gmail.com>
2016-10-10 19:00:58 -0400

    Implemented start and stop of HttpClient

--- server/HttpServer.cs
+++ server/HttpServer.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +10,24 @@ namespace server
 {
     class HttpServer
     {
-        public void _Start() { }
-        public void _Stop() { }
+        public void _Start()
+        {
+            tcpListener = new TcpListener(ipPoint);
+        }
+
+        public void _Stop()
+        {
+            tcpListener.Stop();
+        }
+
         private HttpServerState serverState;
+        public TcpListener tcpListener;
+        public IPEndPoint ipPoint;
+
+        public HttpServer(int port)
+        {
+            ipPoint = new IPEndPoint(IPAddress.Loopback, port);
+        }
+
     }
 }
