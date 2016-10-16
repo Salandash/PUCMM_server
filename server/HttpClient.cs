0e372341141c4f2fbacaede3fe80612a1617f729
William <wjhg03@gmail.com>
2016-10-16 18:47:15 -0400

    Second part of assignment 5 of class

--- server/HttpClient.cs
+++ server/HttpClient.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net.Sockets;
+using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +8,32 @@ using System.Threading.Tasks;
 
 namespace server
 {
-    class HttpClient
+    class HttpClient : IDisposable
     {
-        public void BegnRequest() { }
+        #region Members
         private ClientState stateClient;
+        private bool disposed;
+        private HttpServer _server;
+        private TcpClient _tcp;
+        private static readonly Regex PrologRegex = new Regex("^([A-Z]+) ([^ ]+) (HTTP/[^ ]+)$", RegexOptions.Compiled);
+        #endregion
+
+        public HttpClient(HttpServer serv)
+        {
+            disposed = false;
+            Server = serv;
+        }
+
+        public void BeginRequest() { }
+        
+        void IDisposable.Dispose()
+        {
+            disposed = true;
+        }
+
+        #region Properties
+        public HttpServer Server { get { return _server; } private set { _server = value; } }
+        public TcpClient TcpClient { get { return _tcp; } private set { _tcp = value; } }
+        #endregion
     }
 }
--- server/HttpServer.cs
+++ server/HttpServer.cs
@@ -4,19 +4,36 @@ using System.Net.Sockets;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace server
 {
-    class HttpServer
+    class HttpServer : IDisposable
     {
-        private HttpServerState state;
+        #region Members
+        private HttpServerState _state;
         public TcpListener tcpListener;
         public IPEndPoint ipPoint;
+        private object synLock = new object();
+        private Dictionary<HttpClient, bool> _clients = new Dictionary<HttpClient, bool>();
+        private AutoResetEvent clientsChangedEvent = new AutoResetEvent(false);
+        private int _readBufferSize;
+        private int _writeBufferSize;
+        private TimeSpan _readTime;
+        private TimeSpan _writeTime;
+        private TimeSpan _shutDown;
+        private string _serverBanner;
+        private bool _disposed = false;
+        #endregion
 
         public HttpServer(int port)
         {
             ipPoint = new IPEndPoint(IPAddress.Loopback, port);
+            serverState = new HttpServerState();
+            ReadBufferSize = 4096;
+            WriteBufferSize = 4096;
+
+
         }
 
         #region Methods
@@ -29,10 +46,33 @@ namespace server
         {
             tcpListener.Stop();
         }
+
+        private void BeginAcceptTcpClient() { }
+        private void AcceptTcpClientCallback(IAsyncResult ar) { }
+        private void RegisterClient(HttpServer serv) { }
+
+        void IDisposable.Dispose()
+        {
+            _disposed = true;
+        } 
+
+        private void VerifyState(HttpServerState state)
+        {
+            if (_disposed)
+                throw new ObjectDisposedException(GetType().Name);
+            if (_state != state)
+                throw new InvalidOperationException(String.Format("Expected server to be in the '{0}' state", state));
+        }
         #endregion
 
         #region Properties
-        public HttpServerState serverState { get { return state; } set { state = value; } }
+        public HttpServerState serverState { get { return _state; } set { _state = value; } }
+        public int ReadBufferSize { get { return _readBufferSize; } set { _readBufferSize = value; } }
+        public int WriteBufferSize { get { return _writeBufferSize; } set { _writeBufferSize = value; } }
+        public TimeSpan ReadTimeOut { get { return _readTime; } set { _readTime = value; } }
+        public TimeSpan WriteTimeOut { get { return _writeTime; } set { _writeTime = value; } }
+        public TimeSpan ShutdownTimeOut { get { return _shutDown; } set { _shutDown = value; } }
+        public string ServerBanner { get { return _serverBanner; } set { _serverBanner = value; } }
         #endregion
 
     }
